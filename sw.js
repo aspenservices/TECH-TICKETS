cb9998d4fc4933c5d1c6598122dca4ee82273279
aspenservices <serviceaspen096@gmail.com>
2026-07-08 18:49:33 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * To force update: bump CACHE_VERSION below.
  */
 
-const CACHE_VERSION = "v1.1.99";
+const CACHE_VERSION = "v1.2.1";
 const CACHE_NAME = "aspen-spas-" + CACHE_VERSION;
 
 // How long to wait for the network on an HTML navigation before falling back to
