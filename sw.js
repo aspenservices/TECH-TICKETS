01be6c3af17a9354b5b02f226f1299fe1267e345
aspenservices <serviceaspen096@gmail.com>
2026-07-15 12:32:39 -0500

    v2.0.1: parseo PDF en open invoice + board de cobranza por categoria

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@ try{
  * To force update: bump CACHE_VERSION below.
  */
 
-const CACHE_VERSION = "v2.0.0";
+const CACHE_VERSION = "v2.0.1";
 const CACHE_NAME = "aspen-spas-" + CACHE_VERSION;
 
 // How long to wait for the network on an HTML navigation before falling back to
